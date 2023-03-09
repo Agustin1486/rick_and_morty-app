d8acc3c61c71275cd9bbdd44f13f6ddc8cce816e
Agustin1486 <atunez30@gmail.com>
2023-03-09 19:45:16 -0300

    Actualizacion

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,6 +1,7 @@
+import styles from "./card.module.css"
 export default function Card({name, species, gender, image, onClose}) {
    return (
-      <div>
+      <div className={styles.container}>
          <button onClick={onClose}>X</button>
          <h2>{name}</h2>
          <h2>{species}</h2>
